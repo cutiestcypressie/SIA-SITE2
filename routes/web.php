41187da217837202fc7c8e192861da6e2092b178
cutiestcypressie <bullo.cypress23@gmail.com>
2025-04-06 00:53:46 +0800

    changes

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,32 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->get('/users', ['uses' => 'UserController@getUsers']);
+// User Routes
+$router->group(['prefix' => 'users'], function () use ($router) {
+    $router->get('/users', 'UserController@index');          // Get all users
+    $router->post('/users', 'UserController@store');         // Create user (REST standard uses 'store')
+    $router->get('/users/{id}', 'UserController@show');       // Get single user
+    $router->put('/users/{id}', 'UserController@update');     // Full update
+    $router->patch('/users/{id}', 'UserController@update');   // Partial update
+    $router->delete('/users/{id}', 'UserController@destroy'); // Delete user
+});
 
-    $router->get('/users', 'UserController@index');//get all users records
-    $router->post('/users', 'UserController@addUser');//create new user record
-    $router->get('/users/{id}', 'UserController@show');//get user by id
-    $router->put('/users/{id}', 'UserController@update');//update user record
-    $router->patch('/users/{id}', 'UserController@update');//update user record
-    $router->delete('/users/{id}', 'UserController@delete');//delete record
+// UserJob Routes
+$router->group(['prefix' => 'usersjob'], function () use ($router) {
+    $router->get('/usersjob', 'UserJobController@index');       // Get all jobs
+    $router->get('/usersjob/{id}', 'UserJobController@show');    // Get single job
+});
 
-    //user job routes
-    $router->get('/usersjob', 'UserJobController@index'); // Get all jobs
-    $router->get('/usersjob/{id}', 'UserJobController@show'); // Get job by ID
-    
+$router->get('/debug', function() {
+    return response()->json([
+        'routes' => app()->router->getRoutes(),
+        'env' => [
+            'DB_CONNECTION' => env('DB_CONNECTION'),
+            'APP_DEBUG' => env('APP_DEBUG')
+        ]
+    ]);
+});
+
+$router->group(['prefix' => 'api'], function () use ($router) {
+    $router->post('/users', 'UserController@store');
+});
